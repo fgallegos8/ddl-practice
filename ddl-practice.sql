8c8422d3f85fcf64322d243fcdec078125a85a9e
Francisco Gallegos <fgallegos59@cnm.edu>
2020-03-12 11:09:11 -0600

    created a table using MYSQL

---
+++ ddl-practice.sql
@@ -0,0 +1,47 @@
+Use fgallegos59;
+ALTER DATABASE fgallegos59 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS articleTag;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS author;
+
+CREATE TABLE author(
+    authorId BINARY(16) NOT NULL,
+    authorActivationToken CHAR(32),
+    authorAvatarUrl VARCHAR(128),
+    authorEmail VARCHAR(128) NOT NULL,
+    authorHash CHAR(97) NOT NULL,
+    authorUserName VARCHAR(32) NOT NULL,
+    UNIQUE(authorEmail),
+    UNIQUE(authorUserName),
+    INDEX(authorEmail),
+    PRIMARY KEY(authorId)
+);
+
+CREATE TABLE tag(
+    tagId BINARY(16) NOT NULL,
+    tagName VARCHAR (32) NOT NULL,
+    Primary Key(tagId)
+);
+
+CREATE TABLE article(
+    articleId BINARY(16) NOT NULL,
+    articleAuthorId BINARY(16) NOT NULL,
+    articleContent VARCHAR(4000) NOT NULL,
+    articleDate DATETIME(6) NOT NULL,
+    articleImage VARCHAR(255),
+    INDEX(articleAuthorId),
+    FOREIGN KEY(articleAuthorId) REFERENCES author(authorId),
+    PRIMARY KEY(articleId)
+);
+
+CREATE TABLE articleTag(
+    articleTagArticleId BINARY(16) NOT NULL,
+    articleTagTagId BINARY(16) NOT NULL,
+    INDEX(articleTagArticleId),
+    INDEX(articleTagTagId),
+    FOREIGN KEY(articleTagArticleId) REFERENCES article(articleId),
+    FOREIGN KEY(articleTagTagId) REFERENCES tag(tagId),
+    PRIMARY KEY(articleTagArticleId, articleTagTagId)
+);
